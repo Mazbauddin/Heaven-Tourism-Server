4dccc1664b7d4aadeac6e927add93dd4f7b70b1d
Mazbauddin <mazbauddin2010@gmail.com>
2024-04-30 23:04:20 +0600

    vercel json add

--- index.js
+++ index.js
@@ -6,7 +6,24 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://heaven-tourism-management-site.web.app",
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iua9cew.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -24,47 +41,47 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
-    const tourist_spotCollection = client
-      .db("tourist_spotDB")
-      .collection("tourist_spot");
+    const touristSpotCollection = client
+      .db("touristSpotDB")
+      .collection("touristSpot");
 
     // get tourist spot data work
-    app.get("/tourist_spot", async (req, res) => {
-      const cursor = tourist_spotCollection.find();
+    app.get("/touristSpot", async (req, res) => {
+      const cursor = touristSpotCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
     // post tourist spot data work here
-    app.post("/tourist_spot", async (req, res) => {
+    app.post("/touristSpot", async (req, res) => {
       const newTouristSpot = req.body;
       console.log(newTouristSpot);
-      const result = await tourist_spotCollection.insertOne(newTouristSpot);
+      const result = await touristSpotCollection.insertOne(newTouristSpot);
       res.send(result);
     });
 
     // my cart data work here
     app.get("/myCart/:email", async (req, res) => {
       console.log(req.params.email);
-      const result = await tourist_spotCollection
+      const result = await touristSpotCollection
         .find({ email: req.params.email })
         .toArray();
       res.send(result);
     });
 
     // delete
-    app.delete("/tourist_spot/:id", async (req, res) => {
+    app.delete("/touristSpot/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await tourist_spotCollection.deleteOne(query);
+      const result = await touristSpotCollection.deleteOne(query);
       res.send(result);
     });
 
     // single data  work here
     app.get("/singleSpot/:id", async (req, res) => {
-      const result = await tourist_spotCollection.findOne({
+      const result = await touristSpotCollection.findOne({
         _id: new ObjectId(req.params.id),
       });
       res.send(result);
@@ -87,7 +104,7 @@ async function run() {
           image_Url: req.body.image_Url,
         },
       };
-      const result = await tourist_spotCollection.updateOne(query, data);
+      const result = await touristSpotCollection.updateOne(query, data);
       console.log(result);
       res.send(result);
     });
@@ -96,13 +113,13 @@ async function run() {
     app.delete("/delete/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await tourist_spotCollection.deleteOne(query);
+      const result = await touristSpotCollection.deleteOne(query);
       console.log(result);
       res.send(result);
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
